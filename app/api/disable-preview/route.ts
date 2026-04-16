421cdbb8a8560ad358ed50ea0dfaef7a71bfa13a
cav1947 <andreicernat@sanselo.com>
2026-04-16 08:47:49 +0300

    reparare preview mode

--- app/api/disable-preview/route.ts
+++ app/api/disable-preview/route.ts
@@ -2,6 +2,7 @@ import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  draftMode().disable()
+  const draft = await draftMode()
+  draft.disable()
   redirect('/')
 }
